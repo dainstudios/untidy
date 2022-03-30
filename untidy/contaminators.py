ce69bdb626cbb3a1c7b94baaf8a309165931e1cb
matevaradain <93525955+matevaradain@users.noreply.github.com>
2022-03-30 13:17:52 +0200

    add string spec functions

--- untidy/contaminators.py
+++ untidy/contaminators.py
@@ -35,45 +35,6 @@ def get_data_dims(data):
     return row_idxs, col_idxs
 
 
-def get_random_indices(row_idxs, col_idxs, corruption_level=4):
-    """
-    Get random indeces to contaminate
-
-    Parameters
-    ----------
-    row_idxs: int
-        number of rows in data to contaminate
-    col_idxs: int
-        number of columns in data to contaminate
-    corruption_level int, optional
-        level of corruption, should be between 0 and 10, where 0 leaves the dataset as is, 10
-        is the highest level of contamination
-
-    Returns
-    -------
-    list of indeces to be contaminated
-    """
-
-    # define amount of missing values to introduce
-    missing_count = int(row_idxs * col_idxs * corruption_level / 10)
-
-    # generate randomized missing values indeces
-    nan_idxs = []
-    while len(nan_idxs) < missing_count:
-        if col_idxs > 1:
-            idx_pair = (
-                random.randint(0, row_idxs - 1),
-                random.randint(0, col_idxs - 1),
-            )
-        else:
-            idx_pair = (random.randint(0, row_idxs - 1),)
-
-        if idx_pair not in nan_idxs:
-            nan_idxs.append(idx_pair)
-
-    return nan_idxs
-
-
 """ Functions to contaminate text columns """
 
 
@@ -91,29 +52,78 @@ def add_noise_to_strings(clean_data, corruption_level=4):
 
     Returns
     -------
-    contaminated data as pd.DataFrame
+    data: pd.DataFrame or pd.Series
+        contaminated dataset
     """
     data = clean_data.copy()
 
-    # Get text columns
-    if isinstance(data, pd.DataFrame):
-        str_cols = list(data.select_dtypes(include=["category", "object"]).columns)
-        data = data[str_cols]
-
-    # Get data shape
-    num_rows, num_cols = get_data_dims(data)
+    # Find data cells to contaminate
     idxs_to_contaminate = get_random_indices(
-        num_rows, num_cols, corruption_level=corruption_level
+        data, col_type="str", corruption_level=corruption_level
     )
 
     # Perform contamination
+    noise_chars = "%&$?!#"
+    whitespace = "  "
+    for x, y in idxs_to_contaminate:
+        # add either extra whitespace or a superfluous character
+        noise_char = random.choice(noise_chars)
+        noise = random.choice([noise_char, whitespace])
+        if isinstance(data, pd.DataFrame):
+            data.iloc[x, y] += noise
+        else:
+            data[x] += noise
 
-    return data_contaminated
+    return data
 
 
 def change_str_encoding(clean_data, corruption_level=4):
-    """Changes the string encoding for some columns of a dataframe"""
-    # TODO
+    """
+    Changes the string encoding of text data.
+
+    Parameters
+    ----------
+    clean_data: pd.Series or pd.DataFrame
+        data to be contaminated with superfluous characters
+    corruption_level: int, optional
+        level of corruption, should be between 0 and 10, where 0 leaves the dataset as is, 10
+        is the highest level of contamination
+
+    Returns
+    -------
+    data: pd.DataFrame or pd.Series
+        contaminated dataset
+    """
+    data = clean_data.copy()
+
+    if isinstance(data, pd.DataFrame):
+        # Find random columns to contaminate
+        str_cols = list(data.select_dtypes(include=["object"]).columns)
+        num_str_cols = len(str_cols)
+        possible_num_cols_to_contaminate = [
+            int(np.ceil(n)) for n in np.linspace(0, int(num_str_cols) / 2, 11)
+        ]
+        num_cols_to_contaminate = possible_num_cols_to_contaminate[corruption_level]
+        cols_to_contaminate = list(
+            np.random.choice(str_cols, num_cols_to_contaminate, replace=False)
+        )
+
+        # Change encoding of columns
+        for col in cols_to_contaminate:
+            if corruption_level > 8:
+                data[col] = data[col].str.encode("utf-16")
+            else:
+                data[col] = data[col].str.encode("ascii")
+
+    elif isinstance(data, pd.Series):
+        if corruption_level > 8:
+            data = data.str.encode("utf-16")
+        else:
+            data = data.str.encode("ascii")
+    else:
+        raise ValueError("'clean_data' should be either a pd.DataFrame or pd.Series")
+
+    return data
 
 
 """ Functions to contaminate any column """
@@ -155,9 +165,84 @@ def nan_values(clean_data, corruption_level=4):
 """ Functions to contaminate numerical columns """
 
 
-def change_to_str(clean_data, corruption_level=4):
-    """Changes the dtype of some datapoints in some numeric columns to strings"""
-    # TODO
+def change_numeric_to_str(clean_data, corruption_level=4):
+    """
+    Changes the dtype in some numeric columns to strings
+
+    Parameters
+    ----------
+    clean_data: pd.Series or pd.DataFrame
+        data to be contaminated with superfluous characters
+    corruption_level: int, optional
+        level of corruption, should be between 0 and 10, where 0 leaves the dataset as is, 10
+        is the highest level of contamination
+
+    Returns
+    -------
+    data: pd.DataFrame or pd.Series
+        contaminated dataset
+    """
+    data = clean_data.copy()
+
+    if isinstance(data, pd.DataFrame):
+        # Find random columns to contaminate
+        numeric_cols = list(data.select_dtypes(include=["float64", "in64"]).columns)
+        num_numeric_cols = len(numeric_cols)
+        possible_num_cols_to_contaminate = [
+            int(np.ceil(n)) for n in np.linspace(0, int(num_numeric_cols) / 2, 11)
+        ]
+        num_cols_to_contaminate = possible_num_cols_to_contaminate[corruption_level]
+        cols_to_contaminate = list(
+            np.random.choice(numeric_cols, num_cols_to_contaminate, replace=False)
+        )
+
+        # Change dtype of columns
+        for col in cols_to_contaminate:
+            data[col] = data[col].astype(str)
+
+    elif isinstance(data, pd.Series):
+        data = data.astype(str)
+
+    else:
+        raise ValueError("'clean_data' should be either a pd.DataFrame or pd.Series")
+
+    return data
+
+
+""" Functions to contaminate any column """
+
+
+def nan_values(clean_data, corruption_level=4):
+    """
+    Introduce missing values in clean data
+
+    Parameters
+    ----------
+    clean_data: pd.Series or pd.DataFrame
+        data to be contaminated with missing values
+    corruption_level: int, optional
+        level of corruption, should be between 0 and 10, where 0 leaves the dataset as is, 10
+        is the highest level of contamination
+
+    Returns
+    -------
+    contaminated data as pd.DataFrame
+
+    """
+    data = clean_data.copy()
+
+    row_idxs, col_idxs = data_check(data)
+
+    nan_idxs = random_indices(row_idxs, col_idxs, corruption_level=corruption_level)
+
+    # insert missing values
+    for x, y in nan_idxs:
+        if isinstance(data, pd.DataFrame):
+            data.iloc[x : x + 1, y : y + 1] = np.nan
+        else:
+            data[x] = np.nan
+
+    return data
 
 
 def add_outliers(clean_data, corruption_level=4):
