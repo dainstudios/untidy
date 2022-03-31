a82ab05e10937ff4e953fd179076472acbe7db4c
sinem-unal89 <sinem.nazaroglu@dainstudios.com>
2022-03-31 09:06:21 +0200

    Round the amount of duplicated rows/columsn up to avoid zero change.

--- untidy/contaminators.py
+++ untidy/contaminators.py
@@ -7,6 +7,7 @@ import random
 
 """ Column-wise contaminators """
 
+
 def get_data_dims(data):
     """
     Check input data
@@ -28,14 +29,15 @@ def get_data_dims(data):
         num_cols = 1
 
     elif isinstance(data, pd.DataFrame):
-        num_rows, num_cols = data.shape 
-        
+        num_rows, num_cols = data.shape
+
     else:
-        raise TypeError('data should be pd.Series or pd.DataFrame')
+        raise TypeError("data should be pd.Series or pd.DataFrame")
 
     return num_rows, num_cols
 
-def get_random_indices(num_rows, num_cols, corruption_level = 4):
+
+def get_random_indices(num_rows, num_cols, corruption_level=4):
     """
     Get random indeces to contaminate
 
@@ -55,24 +57,28 @@ def get_random_indices(num_rows, num_cols, corruption_level = 4):
     """
 
     # define amount of missing values to introduce
-    contamination_fraction = round(num_rows * num_cols * corruption_level/10 * 0.6)
+    contamination_fraction = round(num_rows * num_cols * corruption_level / 10 * 0.6)
 
     # generate randomized missing values indeces
-    
-    if num_cols>1:
-        cont_idxs = zip(random.choices(range(num_rows), k=contamination_fraction),
-                    random.choices(range(num_cols), k=contamination_fraction))
+
+    if num_cols > 1:
+        cont_idxs = zip(
+            random.choices(range(num_rows), k=contamination_fraction),
+            random.choices(range(num_cols), k=contamination_fraction),
+        )
     else:
-        cont_idxs = zip(random.choices(range(num_rows), k=contamination_fraction),
-                    [0 for i in range(contamination_fraction)])
+        cont_idxs = zip(
+            random.choices(range(num_rows), k=contamination_fraction),
+            [0 for i in range(contamination_fraction)],
+        )
 
     return cont_idxs
 
 
-
 """ Functions to contaminate any columns: """
 
-def add_nans(clean_data, corruption_level = 4):
+
+def add_nans(clean_data, corruption_level=4):
     """
     Introduce missing values in clean data
 
@@ -91,15 +97,15 @@ def add_nans(clean_data, corruption_level = 4):
     """
     data = clean_data.copy()
     print(data)
-    
+
     num_rows, num_cols = get_data_dims(data)
 
-    nan_idxs = get_random_indices(num_rows, num_cols, corruption_level = corruption_level)
-    
+    nan_idxs = get_random_indices(num_rows, num_cols, corruption_level=corruption_level)
+
     # insert missing values
     for x, y in nan_idxs:
         if isinstance(data, pd.DataFrame):
-            data.iloc[x:x+1, y:y+1] = np.nan
+            data.iloc[x : x + 1, y : y + 1] = np.nan
         else:
             data[x] = np.nan
 
@@ -127,7 +133,9 @@ def contaminate_strings_with_noise(a, corruption_level=4):
 
 
 """ Functions to contaminate numerical columns: """
-def add_outliers(clean_data, corruption_level = 4):
+
+
+def add_outliers(clean_data, corruption_level=4):
     """
     Contaminate data with obvious outliers
 
@@ -143,55 +151,62 @@ def add_outliers(clean_data, corruption_level = 4):
 
     """
 
-    #data_raw = clean_data.copy()
+    # data_raw = clean_data.copy()
     data = clean_data.copy()
 
     print(data)
 
     # get numerical columns
     if isinstance(data, pd.DataFrame):
-        numeric_col = list(data.select_dtypes(include = np.number).columns)
+        numeric_col = list(data.select_dtypes(include=np.number).columns)
         data = data[numeric_col]
-        num_limits = {col:(data[col].min(),data[col].max()) for col in numeric_col}
+        num_limits = {col: (data[col].min(), data[col].max()) for col in numeric_col}
     else:
         num_limits = (data.min(), data.max())
 
     # get data shape
     num_rows, num_cols = get_data_dims(data)
 
-    ol_idxs = get_random_indices(num_rows, num_cols, corruption_level = corruption_level)
-     
+    ol_idxs = get_random_indices(num_rows, num_cols, corruption_level=corruption_level)
+
     # random sign function
     coin_flip = lambda: 1 if random.random() > 0.5 else -1
-    
+
     # contaminate data
     for x, y in ol_idxs:
 
         coin = coin_flip()
-        if coin>0:
+        if coin > 0:
             # +100 times the max(1, max_value)
             if isinstance(data, pd.DataFrame):
-                data.iloc[x:x+1, y:y+1] = num_limits[numeric_col[y]][1] + max(num_limits[numeric_col[y]][1], 1) * 1000
+                data.iloc[x : x + 1, y : y + 1] = (
+                    num_limits[numeric_col[y]][1]
+                    + max(num_limits[numeric_col[y]][1], 1) * 1000
+                )
             else:
                 data[x] = num_limits[1] + max(1, num_limits[1]) * 1000
         else:
             # - 100 times min(-1, min_value)
             if isinstance(data, pd.DataFrame):
-                data.iloc[x:x+1, y:y+1] = num_limits[numeric_col[y]][0] - max(num_limits[numeric_col[y]][0], 1) * 1000
+                data.iloc[x : x + 1, y : y + 1] = (
+                    num_limits[numeric_col[y]][0]
+                    - max(num_limits[numeric_col[y]][0], 1) * 1000
+                )
             else:
                 data[x] = num_limits[0] - max(num_limits[0], 1) * 1000
 
     # add contamination to original dataframe
-    
+
     if isinstance(data, pd.DataFrame):
         data_contaminated = clean_data.copy()
         data_contaminated[numeric_col] = data[numeric_col]
     else:
         data_contaminated = data
-    
+
     print(data_contaminated)
 
     return data_contaminated
+
 
 """ Functions to contaminate string columns: """
 
@@ -216,7 +231,7 @@ def duplicate_rows(data, corruption_level=4):
     Dataset with duplication
     """
     n_rows, _ = data.shape
-    n_rows_duplicated = int(n_rows * (0.6 * corruption_level / 10))
+    n_rows_duplicated = int(np.ceil(n_rows * (0.6 * corruption_level / 10)))
 
     selected_df = data.sample(n=n_rows_duplicated, axis=0, replace=False, weights=None)
 
@@ -240,7 +255,7 @@ def duplicate_columns(data, corruption_level=4):
     Dataset with duplication
     """
     _, n_cols = data.shape
-    n_cols_duplicated = int(n_cols * (0.6 * corruption_level / 10))
+    n_cols_duplicated = int(np.ceil(n_cols * (0.6 * corruption_level / 10)))
 
     selected_df = data.sample(n=n_cols_duplicated, axis=1, replace=False, weights=None)
 
