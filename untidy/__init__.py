5ad230561a363fc843c48cc88b5480ca44c19bed
sinem-unal89 <sinem.nazaroglu@dainstudios.com>
2022-04-01 08:57:43 +0200

    Put imports back to init

---
+++ untidy/__init__.py
@@ -0,0 +1,11 @@
+from untidy.contaminators import (
+     add_duplicate_rows,
+     add_duplicate_columns,
+     add_nans,
+     add_noise_to_strings,
+     add_outliers,
+     change_str_encoding,
+     change_numeric_to_str,
+ )
+
+from untidy.main import untidyfy
